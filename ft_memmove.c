a6e4590cf5345180b500145c9e7cb89fbadd4a75
Alvaro Salguero Munoz <asalguer@c1r11s3.42madrid.com>
2024-10-10 18:47:59 +0200

    arreglo memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,19 +2,15 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+        
-	+:+     */
-/*   By: asalguer <asalguer@student.42.fr>          +#+  +:+      
-	+#+        */
-/*                                                +#+#+#+#+#+  
-	+#+           */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalguer <asalguer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:30:16 by asalguer          #+#    #+#             */
 /*   Updated: 2024/10/08 11:30:16 by asalguer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 void	*ft_memmove(void *dest_str, const void *src_str, size_t n)
 {
@@ -23,6 +19,18 @@ void	*ft_memmove(void *dest_str, const void *src_str, size_t n)
 
 	dest = (unsigned char *)dest_str;
 	sour = (const unsigned char *)src_str;
-	n = 0;
-	return (NULL);
+	if (dest > sour)
+	{
+		dest += n;
+		sour += n;
+		while (n--)
+			*(--dest) = *(--sour);
+
+	}
+	else
+	{
+		while (n--)
+			*dest++ = *sour++;
+	}
+	return (dest_str);
 }
